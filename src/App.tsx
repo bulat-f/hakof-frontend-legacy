37e7258025e1e148371987da95112e53ae2ccb81
Bulat F <write2bulat@gmail.com>
2018-11-04 19:41:55 +0300

    Move from `index.css` to `injectGlobal`

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import HomePage from './containers/HomePage';
+
+const Wrapper = styled.div`
+  margin: auto;
+  max-width: 64rem;
+`;
 
 class App extends React.Component {
   public render() {
     return (
-      <Switch>
-        <Route exact={true} path="/" component={HomePage} />
-      </Switch>
+      <Wrapper>
+        <Switch>
+          <Route exact={true} path="/" component={HomePage} />
+        </Switch>
+      </Wrapper>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { injectGlobal } from 'styled-components';
 import Root from './components/Root';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore, { history } from './store/configureStore';
 
-import './index.css';
+injectGlobal`
+  html {
+    font-size: 16px;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    background: #f0f0f0;
+  }
+
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700|Source+Sans+Pro:400,700,900&subset=cyrillic-ext');
+  h1, h2 {
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+
+  p {
+    font-family: 'Open Sans', sans-serif;
+  }
+`;
 
 const store = configureStore();
 
